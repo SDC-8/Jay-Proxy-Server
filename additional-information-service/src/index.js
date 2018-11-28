f21108ba18df14183a248930288c7100db7d47c4
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2018-11-28 17:44:30 -0600

    Added note for index.js

--- additional-information-service/src/index.js
+++ additional-information-service/src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
-import { PreQuery } from './components/PreQuery.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import { PreQuery } from "./components/PreQuery.js";
 
-const port = 8081;
-
+// Once up on Docker inside EC2, have to go to fully qualified url for container
 const client = new ApolloClient({
-  uri: 'http://hr-fec-otb.us-west-1.elasticbeanstalk.com/graphql',
+  uri: `/graphql`
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <PreQuery />
   </ApolloProvider>,
-  document.getElementById('main'),
+  document.getElementById("main")
 );
