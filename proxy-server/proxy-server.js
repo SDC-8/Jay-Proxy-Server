d0d9daa36ebd41805272e79e7f4110635e6c245d
jaypatrickdeguzman <jaypatrickdeguzman@gmail.com>
2018-11-29 16:31:18 -0600

    Changed index html, and updated new relic js file

--- proxy-server/proxy-server.js
+++ proxy-server/proxy-server.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-
+require("newrelic");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
-const port = 8081;
+const port = 8082;
 
-
-app.enable('trust proxy');
+app.enable("trust proxy");
 app.use(cors());
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../public/index.html`))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../public/index.html`));
 });
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/:houseid', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../public/index.html`))
+app.get("/:houseid", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../public/index.html`));
 });
 
 app.listen(port, () => {
